0cc61489ce4f47cbd201d6355a1803b695e4e9c4
23po <ckodalo@gmail.com>
2024-04-08 11:57:59 +0300

    first commit

---
+++ Masker.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+
+public class Masker {
+
+
+
+    public Map<String> masker (Object object) {
+
+       return
+    }
+
+    public static void main (String[] args) {
+
+
+    }
+
+}
---
+++ Object.java
@@ -0,0 +1,9 @@
+public class Object {
+
+        String first_name;
+        String last_name;
+
+        String date_of_birth;
+
+        String email_address;
+}
